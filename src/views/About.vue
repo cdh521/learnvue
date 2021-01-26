f6720ec8892fdd7bea4108bf01fc373f93604aea
cdh111 <1403796974@163.com>
2021-01-26 16:15:02 +0800

    提价了vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<button @click="reduce">-</button>
+		<span>{{num}}</span>
+		<button @click="add">+</button>
+		<p>{{normal()}}</p>
+		<p>{{computed1}}</p>
+		
+	</div>
 </template>
+<script>
+	export default {
+		data() {
+			return {
+				num:10
+			}
+		},
+		methods: {
+			normal(){
+				console.log("普通方法执行了");
+				return "ppp"
+			},
+			reduce() {
+				this.num--;
+			},
+			add() {
+				this.num++;
+			}
+		},
+		computed:{
+			computed1(){
+				console.log("方法执行了");
+				return "事件检测"+this.num
+			}
+		},
+		watch:{
+			watch1(){
+				console.log("事件检测"+this.num)
+			}
+		}
+	}
+</script>
